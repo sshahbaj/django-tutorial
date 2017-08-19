b7dfc72c6747ffaa0eb175c5f9c0c7ff50b2cb17
sshahbaj <shahbaj007@gmail.com>
2017-08-20 03:56:18 +0530

    Add more views to polls app and wire them into the polls.urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello World!")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
