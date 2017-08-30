8584fa8f492fd725dbae1f03988c734833b635f6
sshahbaj <shahbaj007@gmail.com>
2017-08-30 22:40:42 +0530

    Add test cases for Index View.
    Fix get_queryset of Index VIew.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -2,6 +2,17 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse
+
+
+def create_question(question_text, days):
+    '''
+     Create a question with given 'question_text' and publish the given number of 'days'
+     offset to now (negative for questions published in the past, positive for questions
+     that have yet to be published).
+    '''
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionModelTests(TestCase):
@@ -28,3 +39,63 @@ class QuestionModelTests(TestCase):
         time = timezone.localtime(timezone.now()) - datetime.timedelta(hours=23, minutes=59, seconds=59)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), True)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        '''
+        If no questions exists, an appropriate message is displayed.
+        '''
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        '''
+        Questions with a pub_date in the past are displayed on the index page.
+        '''
+        create_question(question_text='Past Question.', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past Question.>']
+                                 )
+
+    def test_future_question(self):
+        '''
+        Questions with a pub_date in the future are not displayed on the index.
+        '''
+        create_question(question_text='Future Question.', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            []
+        )
+
+
+    def test_future_and_past_question(self):
+        '''
+        Even if both future and past questions exist, only past questions are displayed.
+        '''
+        create_question(question_text='Past Question', days=-30)
+        create_question(question_text='Future Question', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past Question>']
+        )
+
+
+    def test_two_past_questions(self):
+        '''
+        The questions index page may display multiple questions.
+        '''
+        create_question(question_text='Past Question1', days=-30)
+        create_question(question_text='Past Question2', days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past Question2>', '<Question: Past Question1>']
+        )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -10,7 +11,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.localtime(timezone.now())
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
