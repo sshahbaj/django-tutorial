7976089bc59eb0284414a6e8663d16c13a1c7256
sshahbaj <shahbaj007@gmail.com>
2017-08-31 22:03:00 +0530

    Add test cases for Detail View.
    Fix users being able to access the future questions in Detail View.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -99,3 +99,23 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past Question2>', '<Question: Past Question1>']
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_questions(self):
+        '''
+        Detail view of a question with pub_date in future returns a 404 not found.
+        '''
+        future_question = create_question(question_text='Future Question', days=30)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_questions(self):
+        """
+        Detail view of a question with pub_date in past shows the question text.
+        """
+        past_question = create_question(question_text='Past Question', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
